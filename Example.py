77da8957f57be0a84f0579aefe68052768036015
aubinh <aubinh92@gmail.com>
2020-10-27 20:16:12 -0400

    example file

---
+++ Example.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import time
+root = Tk()
+root.geometry('600x200+350+300')
+root.title("python clock designing")
+ 
+timenow=''
+cframe =Frame(root, width=200, height=100, bg='green', relief=GROOVE)
+cframe.pack()
+ 
+clock=Label(cframe, padx=25, pady=40, bd=3, fg= 'dark green',font=('arial',48,'bold'),text= timenow,bg='light green', relief= SUNKEN)
+clock.pack()
+ 
+def timer():
+    global timenow
+    newtime = time.strftime('%H: %M: %S %p')
+    if newtime != timenow:
+        timenow= newtime
+        clock.config(text= timenow)
+    clock.after(200, timer)
+timer()
+ 
+ 
+root.mainloop()
