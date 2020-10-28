3e73ddb1695b09a79fdb3cb9c92ea980126fb86b
aubinh <aubinh92@gmail.com>
2020-10-27 23:26:13 -0400

    added photos and changed font colors

--- Example.py
+++ Example.py
@@ -36,34 +36,40 @@ root.geometry('2000x500+1500+00')
 root.wm_state('zoomed')
 root.overrideredirect(1)
 root.bind('<Escape>',lambda e: root.destroy())
+root.configure(bg='black')
  
 timenow=''
 #cframe =Frame(root, width=1920, height=500, bg='black', relief=GROOVE)
 #cframe.pack()
- 
-clock=Label(root, padx=25, pady=40, bd=3, fg= 'dark green',font=('arial',48,'bold'),text= timenow,bg='gray', relief= SUNKEN)
-clock.grid(row=1,column=1)
 
-clock1=Label(root, padx=25, pady=40, bd=3, fg= 'dark green',font=('arial',48,'bold'),text= "",bg='gray', relief= SUNKEN)
-clock1.grid(row=1,column=2)
+canvas = Canvas(root, width = 150, height = 150)      
+canvas.grid(row=1, column=1)      
+img = PhotoImage(file="logo.JPG")      
+canvas.create_image(20,20, anchor=NW, image=img)
 
-clock2=Label(root, padx=25, pady=40, bd=3, fg= 'dark green',font=('arial',48,'bold'),text= "test",bg='gray', relief= SUNKEN)
-clock2.grid(row=1,column=3)
+clock=Label(root, padx=25, pady=40, bd=3, fg= '#FFCA08',font=('arial',48,'bold'),text= timenow,bg='#292929', relief= SUNKEN)
+clock.grid(row=2,column=1)
 
-clock3=Label(root, padx=25, pady=40, bd=3, fg= 'dark green',font=('arial',48,'bold'),text= "test",bg='gray', relief= SUNKEN)
-clock3.grid(row=1,column=4)
+clock1=Label(root, padx=25, pady=40, bd=3, fg= '#FFCA08',font=('arial',48,'bold'),text= "",bg='#292929', relief= SUNKEN)
+clock1.grid(row=2,column=2)
 
-clock4=Label(root, padx=25, pady=40, bd=3, fg= 'dark green',font=('arial',48,'bold'),text= "both",bg='gray', relief= SUNKEN)
-clock4.grid(row=2,column=1)
+clock2=Label(root, padx=25, pady=40, bd=3, fg= '#FFCA08',font=('arial',48,'bold'),text= "test",bg='#292929', relief= SUNKEN)
+clock2.grid(row=2,column=3)
 
-clock5=Label(root, padx=25, pady=40, bd=3, fg= 'dark green',font=('arial',48,'bold'),text= "test",bg='gray', relief= SUNKEN)
-clock5.grid(row=2,column=2)
+clock3=Label(root, padx=25, pady=40, bd=3, fg= '#FFCA08',font=('arial',48,'bold'),text= "test",bg='#292929', relief= SUNKEN)
+clock3.grid(row=2,column=4)
 
-clock6=Label(root, padx=25, pady=40, bd=3, fg= 'dark green',font=('arial',48,'bold'),text= "test",bg='gray', relief= SUNKEN)
-clock6.grid(row=2,column=3)
+clock4=Label(root, padx=25, pady=40, bd=3, fg= '#FFCA08',font=('arial',48,'bold'),text= "both",bg='#292929', relief= SUNKEN)
+clock4.grid(row=3,column=1)
 
-clock7=Label(root, padx=25, pady=40, bd=3, fg= 'dark green',font=('arial',48,'bold'),text= "test",bg='gray', relief= SUNKEN)
-clock7.grid(row=2,column=4)
+clock5=Label(root, padx=25, pady=40, bd=3, fg= '#FFCA08',font=('arial',48,'bold'),text= "test",bg='#292929', relief= SUNKEN)
+clock5.grid(row=3,column=2)
+
+clock6=Label(root, padx=25, pady=40, bd=3, fg= '#FFCA08',font=('arial',48,'bold'),text= "test",bg='#292929', relief= SUNKEN)
+clock6.grid(row=3,column=3)
+
+clock7=Label(root, padx=25, pady=40, bd=3, fg= '#FFCA08',font=('arial',48,'bold'),text= "test",bg='#292929', relief= SUNKEN)
+clock7.grid(row=3,column=4)
 
 def timezoner(hours):
     global timenow
@@ -79,11 +85,11 @@ def timer():
         clock.config(text= timenow)
         clock1.config(text= timezoner(clock1_zone))
         clock2.config(text= timezoner(clock2_zone))
-        #clock3.config(text= timezoner(clock3_zone))
-        #clock4.config(text= timezoner(clock4_zone))
-        #clock5.config(text= timezoner(clock5_zone))
-        #clock6.config(text= timezoner(clock6_zone))
-        #clock7.config(text= timezoner(clock7_zone))
+        clock3.config(text= timezoner(clock3_zone))
+        clock4.config(text= timezoner(clock4_zone))
+        clock5.config(text= timezoner(clock5_zone))
+        clock6.config(text= timezoner(clock6_zone))
+        clock7.config(text= timezoner(clock7_zone))
     clock.after(200, timer)
 timer()
  
